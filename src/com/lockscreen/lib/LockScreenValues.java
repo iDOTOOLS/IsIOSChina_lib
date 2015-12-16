a645d599a5c985320f8ccc431bc9c7ed1f2f7515
daihuido <daihui@idotools.com>
2015-12-16 17:30:48 +0800

    add hipad branch

--- src/com/lockscreen/lib/LockScreenValues.java
+++ src/com/lockscreen/lib/LockScreenValues.java
@@ -4,5 +4,6 @@ public class LockScreenValues {
 
     public static final boolean isPublishInChina      = true;
     public static final boolean isIOS8LockscreenBranch = true;
+    public static final boolean isPublishInHipad = false;
 
 }
