5a671816212f05b133003cfd37b147a72e96d433
daihuido <daihui@idotools.com>
2016-01-04 15:38:21 +0800

    d

--- src/com/lockscreen/lib/LockScreenValues.java
+++
@@ -1,9 +0,0 @@
-package com.lockscreen.lib;
-
-public class LockScreenValues {
-
-    public static final boolean isPublishInChina      = true;
-    public static final boolean isIOS8LockscreenBranch = true;
-    public static final boolean isPublishInHipad = false;
-
-}
