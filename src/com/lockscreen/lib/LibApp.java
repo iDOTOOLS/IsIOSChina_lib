1dfc95a4d91f02b00300fb4b5491355b05654c5b
kaixuan <likaixuan@idotools.com>
2016-01-04 18:25:32 +0800

    iso

--- src/com/lockscreen/lib/LibApp.java
+++ src/com/lockscreen/lib/LibApp.java
@@ -1,10 +1,13 @@
 package com.lockscreen.lib;
 
+import com.dotools.toolbox.AddTool;
+import com.lockscreen.api.ToolBoxAPI;
 import com.lockscreen.api.UmengUpdateAPI;
 
 public class LibApp {
 
     public static void init(){
         UmengUpdateAPI.className = UmengUpdate.class.getName();
+        ToolBoxAPI.className = AddTool.class.getName();
     }
 }
